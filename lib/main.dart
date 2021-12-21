2a1bf25c205cd42b1ea3457752acd16246f10ce8
silverz <liisa9601@naver.com>
2021-12-21 20:51:11 +0900

    로그인 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'dice.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +21,9 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,58 +36,115 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage('image/chef.gif'),
-                width: 170.0,
-                height: 190.0,
-              ),
-            ),
-            Form(
-                child: Theme(
-              data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                      labelStyle:
-                          TextStyle(color: Colors.teal, fontSize: 15.0))),
-              child: Container(
-                padding: EdgeInsets.all(70.0),
-                child: Column(
-                  children: [
-                    TextField(
-                        decoration: InputDecoration(labelText: 'Enter ID'),
-                        keyboardType: TextInputType.emailAddress),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Enter Password'),
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    ButtonTheme(
-                        minWidth: 100.0,
-                        height: 50.0,
-                        child: RaisedButton(
-                          color: Colors.orangeAccent,
-                          child: Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                            size: 35.0,
-                          ),
-                          onPressed: () {},
-                        ))
-                  ],
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Center(
+                  child: Image(
+                    image: AssetImage('image/chef.gif'),
+                    width: 170.0,
+                    height: 190.0,
+                  ),
                 ),
-              ),
-            ))
-          ],
-        ),
+                Form(
+                    child: Theme(
+                  data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                          labelStyle:
+                              TextStyle(color: Colors.teal, fontSize: 15.0))),
+                  child: Container(
+                    padding: EdgeInsets.all(70.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                            controller: controller,
+                            decoration: InputDecoration(labelText: 'Enter ID'),
+                            keyboardType: TextInputType.emailAddress),
+                        TextField(
+                          controller: controller2,
+                          decoration:
+                              InputDecoration(labelText: 'Enter Password'),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        ButtonTheme(
+                            minWidth: 100.0,
+                            height: 50.0,
+                            child: RaisedButton(
+                              color: Colors.orangeAccent,
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                                size: 35.0,
+                              ),
+                              onPressed: () {
+                                if (controller.text == 'dice' &&
+                                    controller2.text == '1234') {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              Dice()));
+                                } else if (controller.text == 'dice' &&
+                                    controller2.text != '1234') {
+                                  showSnackBar2(context);
+                                } else if (controller.text != 'dice' &&
+                                    controller2.text == '1234') {
+                                  showSnackBar3(context);
+                                } else {
+                                  showSnackBar(context);
+                                }
+                              },
+                            ))
+                      ],
+                    ),
+                  ),
+                ))
+              ],
+            ),
+          );
+        },
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '로그인 정보를 확인하세요.',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '비밀번호가 일치하지 않습니다.',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '아이디를 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
